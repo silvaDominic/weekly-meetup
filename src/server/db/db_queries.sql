77171444e8bd3ce402cd6420eea63186032761b0
Dominic Silva <silva.d.dominic@gmail.com>
2021-11-22 15:13:21 -0500

    Added db queries

---
+++ src/server/db/db_queries.sql
@@ -0,0 +1,48 @@
+CREATE SCHEMA meetup_db;
+USE meetup_db;
+
+CREATE TABLE users (
+	id VARCHAR(36) NOT NULL,
+    email VARCHAR(254) NOT NULL UNIQUE,
+    display_name VARCHAR(35) NOT NULL,
+    password VARCHAR(36) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE events (
+	id VARCHAR(36) NOT NULL,
+    create_at TIMESTAMP DEFAULT NOW(),
+    
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE rsvps (
+	event_id VARCHAR(36) NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
+    
+	PRIMARY KEY (event_id, user_id),
+    FOREIGN KEY (event_id) REFERENCES events (id),
+    FOREIGN KEY (user_id) REFERENCES events (id)
+);
+
+CREATE TABLE topics (
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(500),
+    event_id VARCHAR(36) NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
+    
+    PRIMARY KEY (event_id, user_id),
+    FOREIGN KEY (event_id) REFERENCES events (id),
+    FOREIGN KEY (user_id) REFERENCES events (id)
+);
+
+CREATE TABLE participants (
+    event_id VARCHAR(36) NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
+    
+	PRIMARY KEY (event_id, user_id),
+	FOREIGN KEY (event_id) REFERENCES events (id),
+    FOREIGN KEY (user_id) REFERENCES events (id)
+);
