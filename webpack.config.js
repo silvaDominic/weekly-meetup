2514f80d98b31f6da2d2f2db07533d347e43a378
Dominic Silva <silva.d.dominic@gmail.com>
2021-12-25 00:49:14 -0500

    Added base styles and style variables to project

--- webpack.config.js
+++ webpack.config.js
@@ -2,67 +2,82 @@ import path from 'path';
 import Dotenv from 'dotenv-webpack';
 import nodeExternals from 'webpack-node-externals';
 
+// eslint-disable-next-line no-underscore-dangle
 const __dirname = path.resolve();
 
 const serverConfig = {
-    mode: process.env.NODE_ENV || 'development',
-    entry: './src/server/server.ts',
-    plugins: [
-        new Dotenv(),
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/server/server.ts',
+  plugins: [
+    new Dotenv(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          configFile: 'tsconfig.server.json',
+        },
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-                options: {
-                    configFile: 'tsconfig.server.json'
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    output: {
-        filename: 'server.cjs',
-        path: path.resolve(__dirname, 'dist')
-    },
-    target: 'node',
-    node: {
-        __dirname: false
-    },
-    externals: [nodeExternals()]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'server.cjs',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'node',
+  node: {
+    __dirname: false,
+  },
+  externals: [nodeExternals()],
 };
 
 const clientConfig = {
-    mode: process.env.NODE_ENV || 'development',
-    entry: './src/client/index.tsx',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-                options: {
-                    configFile: 'tsconfig.client.json'
-                }
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/client/index.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          configFile: 'tsconfig.client.json',
+        },
+      },
+      {
+        test: /\.scss|.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              sourceMap: true,
+              resources: [
+                './src/client/meetup/presentation/styles/variables/index.scss',
+                './src/client/meetup/presentation/styles/utils.scss',
+              ],
             },
-            {
-                test: /\.scss|.css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.css', '.scss']
-    },
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'public/js')
-    }
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.css', '.scss'],
+  },
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'public/js'),
+  },
 };
 
 export default [serverConfig, clientConfig];
