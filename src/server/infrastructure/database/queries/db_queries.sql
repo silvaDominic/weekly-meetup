b043fe1cca5e88e2b3dd23ae98f285b5e7ed9114
Dominic Silva <silva.d.dominic@gmail.com>
2021-12-27 17:30:03 -0500

    Changed DB Schema and adjusted user queries accordingly

--- src/server/infrastructure/database/queries/db_queries.sql
+++ src/server/infrastructure/database/queries/db_queries.sql
@@ -1,8 +1,8 @@
 CREATE SCHEMA meetup_db;
 USE meetup_db;
 
-CREATE TABLE users (
-	id VARCHAR(36) NOT NULL,
+CREATE TABLE user (
+	id MEDIUMINT NOT NULL AUTO_INCREMENT,
     email VARCHAR(254) NOT NULL UNIQUE,
     display_name VARCHAR(35) NOT NULL,
     password CHAR(60) NOT NULL,
@@ -12,37 +12,37 @@ CREATE TABLE users (
 );
 
 CREATE TABLE meetup (
-	id VARCHAR(36) NOT NULL,
+	id MEDIUMINT NOT NULL AUTO_INCREMENT,
     create_at TIMESTAMP DEFAULT NOW(),
 
     PRIMARY KEY (id)
 );
 
-CREATE TABLE rsvps (
-	event_id VARCHAR(36) NOT NULL,
-    user_id VARCHAR(36) NOT NULL,
+CREATE TABLE rsvp (
+	event_id MEDIUMINT NOT NULL,
+    user_id MEDIUMINT NOT NULL,
 
 	PRIMARY KEY (event_id, user_id),
     FOREIGN KEY (event_id) REFERENCES meetup (id),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
-CREATE TABLE topics (
+CREATE TABLE topic (
     title VARCHAR(100) NOT NULL,
     description VARCHAR(500),
-    event_id VARCHAR(36) NOT NULL,
-    user_id VARCHAR(36) NOT NULL,
+    event_id MEDIUMINT NOT NULL,
+    user_id MEDIUMINT NOT NULL,
 
     PRIMARY KEY (event_id, user_id),
     FOREIGN KEY (event_id) REFERENCES meetup (id),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
-CREATE TABLE participants (
-    event_id VARCHAR(36) NOT NULL,
-    user_id VARCHAR(36) NOT NULL,
+CREATE TABLE participant (
+    event_id MEDIUMINT NOT NULL,
+    user_id MEDIUMINT NOT NULL,
 
 	PRIMARY KEY (event_id, user_id),
 	FOREIGN KEY (event_id) REFERENCES meetup (id),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    FOREIGN KEY (user_id) REFERENCES user (id)
 );
--- src/server/infrastructure/database/queries/users.query.ts
+++ src/server/infrastructure/database/queries/users.query.ts
@@ -1,51 +1,55 @@
-import { v4 as uuidv4 } from 'uuid';
 import { IDbQuery } from '../models/db-query.interface';
-import { UserEntity } from '../../../domain/models/users/user.entity';
 import { IUsersTable } from '../models/db.interface';
 
 function insertUser(email: string, displayName: string, password: string): IDbQuery {
   return {
-    command: 'INSERT INTO users (id, email, display_name, password) VALUES (?, ?, ?, ?)',
-    arguments: [uuidv4(), email, displayName, password],
+    command: 'INSERT INTO user (email, display_name, password) VALUES (?, ?, ?)',
+    arguments: [email, displayName, password],
   };
 }
 
 function selectAllUsers(): IDbQuery {
   return {
-    command: 'SELECT * FROM users;',
+    command: 'SELECT * FROM user;',
   };
 }
 
 function selectUser(id: string): IDbQuery {
   return {
-    command: 'SELECT * FROM users WHERE id = ?',
+    command: 'SELECT * FROM user WHERE id = ?',
     arguments: [id],
   };
 }
 
-function findUser(column: string, value: string) {
+function findUser(column: string, value: string): IDbQuery {
   return {
-    command: 'SELECT * FROM users WHERE ?? = ?',
+    command: 'SELECT * FROM user WHERE ?? = ?',
     arguments: [column, value],
   };
 }
 
-function updateUser(id: string, email: string, password: string, displayName: string) {
+function updateUser(id: string, email: string, password: string, displayName: string): IDbQuery {
   const queryObject: IUsersTable = {};
   if (email !== undefined) queryObject.email = email;
   if (password !== undefined) queryObject.password = password;
   if (displayName !== undefined) queryObject.display_name = displayName;
 
   return {
-    command: 'UPDATE users SET ? WHERE id = ?',
+    command: 'UPDATE user SET ? WHERE id = ?',
     arguments: [queryObject, id],
   };
 }
 
-function deleteUser(id: string) {
+function deleteUser(id: string): IDbQuery {
   return {
-    command: 'DELETE FROM users WHERE id = ?',
+    command: 'DELETE FROM user WHERE id = ?',
     arguments: [id],
+  };
+}
+
+function getLastInsertId(): IDbQuery {
+  return {
+    command: 'SELECT LAST_INSERT_ID()',
   };
 }
 
@@ -56,4 +60,5 @@ export const userQueries = {
   findUser,
   updateUser,
   deleteUser,
+  getLastInsertId,
 };
