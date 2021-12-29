572aca2daef77405d75b5a96417a9054ea3c8bf2
Dominic Silva <silva.d.dominic@gmail.com>
2021-12-29 01:25:09 -0500

    Removed deconstruction from environmental variables (no longer works)

--- src/server/libs/constants/environement.const.ts
+++ src/server/libs/constants/environement.const.ts
@@ -1,10 +1,11 @@
-export const { API_ROOT } = process.env;
-export const { PORT } = process.env;
-export const { NODE_ENV } = process.env;
+/* eslint-disable prefer-destructuring */
+export const API_ROOT = process.env.API_ROOT;
+export const PORT = process.env.PORT;
+export const NODE_ENV = process.env.NODE_ENV;
 
-export const { HOST_NAME } = process.env;
-export const { DB_USER } = process.env;
-export const { DB_PASSWORD } = process.env;
-export const { DB_NAME } = process.env;
+export const HOST_NAME = process.env.HOST_NAME;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_NAME = process.env.DB_NAME;
 
-export const { JWT_SECRET } = process.env;
+export const JWT_SECRET = process.env.JWT_SECRET;
